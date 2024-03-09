7a51d020f70090d9823b5c0a6722c35af2417a91
OddestOfAllDean <alideanbaskoro@gmail.com>
2024-03-09 09:08:46 +0700

    Very unprofessionalgit status you have been warned!

--- method.php
+++ method.php
@@ -3,22 +3,26 @@ class Manusia {
 
     public $nama;
     public $umur;
+    public $hobby;
+    public $pacar;
 
-    public function __construct($nama, $umur) {
+    public function __construct($nama, $umur, $hobby, $pacar) {
         $this->nama = $nama;
         $this->umur = $umur;
+        $this->hobby = $hobby;
+        $this->pacar = $pacar;
 
     }
 
     public function getInfo() {
-        return "Nama: " . $this->nama . "<br> Umur: " . $this->umur;
+        return "Nama: " . $this->nama . "<br> Umur: " . $this->umur . " <br>Dan hobby saya yaitu " . $this->hobby;
     }
     public function sapa() {
-        return "Halo, nama saya " . $this->nama;
+        return "Nama pacar saya yaitu " . $this->pacar;
     }
 }
 
-$orang = new Manusia("Asmi Sapkiaki", 18);
+$orang = new Manusia("Asmi Sapkiaki", 18, "Ngewe sama asmarelda", "Asmarelda Capciaki Putri Kencani");
 
 echo $orang->getInfo() . "<br>";
 
